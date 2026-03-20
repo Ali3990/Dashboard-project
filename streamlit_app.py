e7a30eb01cc754f620901f165cd5449115c6cae9
AliMMCR <ali@mmcrealestate.com>
2026-03-20 16:40:26 -0700

    remove wide on the chart space

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import plotly.graph_objects as go
 import os
 from job_report_dash.jolts_chart.jolts_unemp import create_jolts_unemp_chart
 
-st.set_page_config(layout="wide")
 
 fig = create_jolts_unemp_chart()
 st.plotly_chart(fig, use_container_width=True)
